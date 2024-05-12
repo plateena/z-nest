029829e253e9d33396912ae63efe205fad0de2c4
zainundin <zaiundin.mohammad@gmail.com>
2024-05-12 18:15:11 +0800

    feat: config postgres config and connection

--- app/src/src/app.module.ts
+++ app/src/src/app.module.ts
@@ -1,22 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common'
+import { AppController } from '@/app.controller'
+import { AppService } from '@/app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { getTypeOrmConfig } from '@/config/typeorm.config'
 
 @Module({
-  imports: [
-      TypeOrmModule.forRoot({
-          type: 'postgres',
-          host: 'db',
-          port: 5432,
-          username: 'nestjs',
-          password: 'secret',
-          database: 'MOTOR_INSURANCE_WEBSITE',
-          entities: [],
-          synchronize: true,
-      })
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async () => await getTypeOrmConfig(),
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
---
+++ app/src/src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
+export const getTypeOrmConfig = async (): Promise<TypeOrmModuleOptions> => {
+    return {
+        type: process.env.DB_CONNECTION as 'postgres' | 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [],
+        synchronize: true,
+    }
+}
