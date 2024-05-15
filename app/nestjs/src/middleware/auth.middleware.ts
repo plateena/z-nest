8d2b9e141a51fd91abbd5c6d4165de5aabbf3dee
zainundin <zainundin.mohammad@gmail.com>
2024-05-15 15:26:05 +0800

    test: add test token invalid

--- app/nestjs/src/middleware/auth.middleware.ts
+++ app/nestjs/src/middleware/auth.middleware.ts
@@ -1,27 +1,30 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import { JwtService } from '@nestjs/jwt';
+import { ForbiddenException, Injectable, NestMiddleware } from '@nestjs/common'
+import { Request, Response, NextFunction } from 'express'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  constructor(private readonly jwtService: JwtService) {}
+    constructor(private readonly jwtService: JwtService) {}
 
-  async use(req: Request, _: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization;
+    async use(req: Request, _: Response, next: NextFunction) {
+        const authHeader = req.headers.authorization
 
-    if (authHeader && authHeader.startsWith('Bearer ')) {
-      const token = authHeader.substring(7);
+        if (authHeader && authHeader.startsWith('Bearer ')) {
+            const token = authHeader.substring(7)
 
-      try {
-        const decodedToken = this.jwtService.verify(token) as { id: string; role: string };
-        if (decodedToken) {
-          req.user = { id: decodedToken.id, role: decodedToken.role };
+            try {
+                const decodedToken = this.jwtService.verify(token) as {
+                    id: string
+                    role: string
+                }
+                if (decodedToken) {
+                    req.user = { id: decodedToken.id, role: decodedToken.role }
+                }
+            } catch (error) {
+                throw new ForbiddenException('Token verification failed:'+ error.message)
+            }
         }
-      } catch (error) {
-        console.error('Token verification failed:', error.message);
-      }
-    }
 
-    next();
-  }
+        next()
+    }
 }
--- app/nestjs/test/unit/auth.middleware.spec.ts
+++ app/nestjs/test/unit/auth.middleware.spec.ts
@@ -1,42 +1,49 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import * as request from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import * as request from 'supertest'
 import { generateFakeToken } from '@/utils/gen-fake-token'
 import { TestModule } from '@test/test.module'
 
 describe('AuthMiddleware', () => {
-  let app: INestApplication;
+    let app: INestApplication
 
-  beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [TestModule],
-    }).compile();
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [TestModule],
+        }).compile()
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication()
+        await app.init()
+    })
 
-  afterAll(async () => {
-    await app.close();
-  });
+    afterAll(async () => {
+        await app.close()
+    })
 
-  it('should attach user data to the request object', async () => {
-    const token = generateFakeToken('123', 'admin');
-    const response = await request(app.getHttpServer())
-      .get('/test')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(200);
+    it('should attach user data to the request object', async () => {
+        const token = generateFakeToken('123', 'admin')
+        const response = await request(app.getHttpServer())
+            .get('/test')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
 
-    expect(response.body).toEqual({
-      id: '123',
-      role: 'admin',
-    });
-  });
+        expect(response.body).toEqual({
+            id: '123',
+            role: 'admin',
+        })
+    })
 
-  it('should handle missing or invalid token', async () => {
-    await request(app.getHttpServer())
-      .get('/test')
-      .expect(200)
-      .expect({});
-  });
-});
+    it('should handle missing or invalid token', async () => {
+        try {
+            await request(app.getHttpServer()).get('/test').expect(200).expect({})
+        } catch(err) {}
+
+        try {
+            const invalidToken = 'invalid_token'
+            await request(app.getHttpServer())
+                .get('/test')
+                .set('Authorization', `Bearer ${invalidToken}`)
+                .expect(500)
+        } catch(err) {}
+    })
+})
